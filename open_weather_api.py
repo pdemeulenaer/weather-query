fd1d0b8bf70360af37e72ce5defb80594b97765d
pdemeulenaer <pdemeulenaer@gmail.com>
2020-07-20 19:23:29 +0300

    Current weather function created

--- open_weather_api.py
+++ open_weather_api.py
@@ -1,22 +1,36 @@
 # This file is meant to contain all examples of successful calls to open weather api, 
 # following https://rapidapi.com/community/api/open-weather-map?endpoint=53aa6041e4b00287471a2b62 
 
-
-# Current Weather Data
 import requests
 
-url = "https://community-open-weather-map.p.rapidapi.com/weather"
+city_list = ['london','berlin','brussels','vilnius']
 
-querystring = {"callback":"test","id":"2172797","units":"%22metric%22 or %22imperial%22","mode":"xml%2C html","q":"london"}
 
-headers = {
-    'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
-    'x-rapidapi-key': "84a315f297msh04bc1c02d9bf9afp123b82jsn2ba923f95520"
-    }
+def request_weather(city):
+    """Current Weather Data
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+    Args:
+        city (string): name of the input city
 
-print(response.text)
+    Returns:
+        response: response as json object
+    """
+    url = "https://community-open-weather-map.p.rapidapi.com/weather"
+
+    #querystring = {"callback":"test","id":"2172797","units":"%22metric%22 or %22imperial%22","mode":"xml%2C html","q":"london"}
+    querystring = {"callback":"test","units":"metric","mode":"xml%2C html","q":city}
+
+    headers = {
+        'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
+        'x-rapidapi-key': "84a315f297msh04bc1c02d9bf9afp123b82jsn2ba923f95520"
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    return response
+
+response = request_weather(city_list[0])
+print(city_list[0], response.text)
 
 
 '''
